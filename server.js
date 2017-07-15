a546320596752b7b6a3c162f27040114a0b3ddd1
Julien Thoma <julien.thoma89@gmail.com>
2017-07-15 11:57:37 +0200

    post

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const request = require('request');
 const app = express();
  
 app.use(cors());
@@ -10,8 +11,12 @@ const port = process.env.PORT || 3000;
 app.listen(port);
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('test');
+app.post('/', ({ target, body }, res) => {
+    request.post(target,
+    body,
+    (_req, _res) => {
+        res.send(_res);
+    });
 });
 
 module.exports = app;
